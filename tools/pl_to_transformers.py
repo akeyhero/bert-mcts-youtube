d9f9cbae1641572f9db3bc92a98ee2583df1ace2
akihiro <akihiro@kats.la>
2025-04-16 22:16:10 +0900

    pl_to_transformers を ModernBERT に対応

--- tools/pl_to_transformers.py
+++ tools/pl_to_transformers.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 import torch
 
-from src.model.bert import config
+from src.model.bert import config as bert_config
+from src.model.modern_bert import config as modern_bert_config
 
 
 def argparse():
     parser = ArgumentParser(description='Convert pl checkpoint to transformers format')
     parser.add_argument('ckpt_path', type=str)
+    parser.add_argument('--model_arch', type=str, default=None)
     args, _ = parser.parse_known_args()
     return args
 
@@ -24,6 +26,10 @@ def main(args):
     # 同一ディレクトリにpytorch_model.binとconfig.jsonが必要
     state_dict_path = ckpt_dir / f'pytorch_model.bin'
     torch.save(state_dict, state_dict_path)
+    if args.model_arch == 'modernbert':
+        config = modern_bert_config
+    else:
+        config = bert_config
     config.to_json_file(ckpt_dir / 'config.json')
 
 
